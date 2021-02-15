a6bf5264114c212f80a8820589eca96106d71263
mfranl <mflandry09@gmail.com>
2021-02-15 10:47:50 -0700

    added my question array

--- index.js
+++ index.js
@@ -10,10 +10,58 @@ const inquirer = require('inquirer');
 const questions = [
     {
         type:"input",
-        message:"",
-        name:""
-    }
+        message:"Project Title:",
+        name:"Title"
+    },
 
+    {
+        type:"input",
+        message:"Project Description:",
+        name:"Describe"
+    },
+
+    {
+        type:"input",
+        message:"Installation Guidelines:",
+        name:"Install"
+    },
+
+    {
+        type:"input",
+        message:"How to Use:",
+        name:"Usage"
+    },
+
+    {
+        type:"input",
+        message:"Contribution Guidelines:",
+        name:"Contribute"
+    },
+    
+    {
+        type:"input",
+        message:"Tests to run:",
+        name:"Tests"
+    },
+
+    {
+        type:"list",
+        message:"License used:",
+        choices:['MIT', 'GNU GPLv3', 'Apache 2.0', 'none'],
+        name:"License"
+    },
+
+    {
+        type:"input",
+        message:"GitHub URL:",
+        name:"Github"
+    },
+
+    {
+        type:"input",
+        message:"Email address:",
+        name:"Email"
+    }
 ];
 
 // TODO: Create a function to write README file
